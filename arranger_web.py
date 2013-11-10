557c7319553daf33302464d5fad371f765dc0981
David DesRoches <delicious@echonest.com>
2013-11-10 13:59:33 -0500

    Choose file button and dialog.

--- arranger_web.py
+++ arranger_web.py
@@ -10,6 +10,12 @@ HEAD = """
 <title>The Lone Arranger</title>
 <head>
 <h1>The Lone Arranger</h1>
+<script>
+function file_chosen()
+{
+    document.getElementById("filename").click();
+}
+</script>
 </head>
 <body>
 BODY_HERE
@@ -32,25 +38,28 @@ class Arranger(object):
         return HEAD.replace("BODY_HERE", open("body.html").read())
     index.exposed = True
 
-    def load(self):
-        # TODO: Choose local file to remix.
-        self.engine = Engine("/audio/wto.mp3")
-        return
-    load.exposed = True
-
     def play(self):
         engine.play(self.section_list)
     play.exposed = True
     
     def arrange(self, section_list):
-        engine.arrange(section_list)
-
-        self.sections = engine.get_sections()
+        engine = Engine(document.getElementById("filename").name)
+        #engine.arrange(section_list)
+        #self.sections = engine.get_sections()
+        self.sections = [(0.0, 8.1030800000000003)
+            (8.1030800000000003, 17.119399999999999)
+            (25.222480000000001, 10.52399)
+            (35.746470000000002, 16.750139999999998)
+            (52.496609999999997, 11.51864)
+            (64.015249999999995, 18.80688)
+            (82.822119999999998, 12.914199999999999)
+            (95.736320000000006, 18.047529999999998)
+            (113.78385, 35.805979999999998)
+            (149.58983000000001, 15.222060000000001)
+            (164.81189000000001, 21.20607)]
 
         # 'sections' is a list of dicts like [{'start': <time>, 'duration': <time>}, etc...]
-
         body = self._make_arrangement_table(self.sections)
-
         return HEAD.replace("BODY_HERE", body)
     arrange.exposed = True
 
@@ -65,10 +74,10 @@ class Arranger(object):
 
     def _make_arrangement_table(self, sections):
         # Start the table
-        table = '<table border="1"><tr>'
-        totalDuration = sum([s['duration'] for s in sections])
+        table = '<table border="1"><tr width=1000>'
+        totalDuration = sum([s[1] for s in sections])
         for s in sections:
-            table += '<td bgcolor=red width=%d' % _get_width(s['duration'], totalDuration) + '</td>'
+            table += '<td bgcolor=red width=%d' % 1000.0 / (s[0] / totalDuration) + '</td>'
             #table += '<td><iframe src="https://embed.spotify.com/?uri=spotify:track:%s" frameborder="0" width="400" height="80" allowtransparency="true"></iframe></td>' % t["foreign_id"]
 
         table += '</tr>'
